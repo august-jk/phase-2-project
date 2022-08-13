9731ba5335231e75e05f193a4ce55df4c74bd06b
Aletha Wheeless <augustkama98@gmail.com>
2022-08-13 13:13:46 -1000

    set up inventory list with css

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useEffect, useState } from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Navbar from './Navbar';
+import Home from './Home';
+import Inventory from './Inventory';
+import Form from './Form';
+
 
 function App() {
+  const [parts, setParts] = useState([])
+  useEffect(() => {
+    fetch('http://localhost:3000/parts')
+    .then(r => r.json())
+    .then(data => setParts(data))
+  }, [])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      <Routes>
+        <Route path='/inventory' element={<Inventory parts={parts}/>}>
+        </Route>
+        <Route path='/form' element={<Form />}>
+        </Route>
+        <Route path='/' element={<Home />}>
+        </Route>
+      </Routes>  
     </div>
   );
 }
