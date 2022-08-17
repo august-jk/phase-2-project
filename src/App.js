80b45b0b07a4f89e3fe41eb9ee58cfadfbe91a7a
Aletha Wheeless <augustkama98@gmail.com>
2022-08-17 10:21:42 -1000

    add markup price

--- src/App.js
+++ src/App.js
@@ -9,16 +9,27 @@ import Form from './Form';
 
 function App() {
   const [parts, setParts] = useState([])
+  const [search, setSearch] = useState('')
   useEffect(() => {
     fetch('http://localhost:3000/parts')
     .then(r => r.json())
     .then(data => setParts(data))
   }, [])
+  const updatedParts = parts.filter(part => {
+    if(part.name.toLowerCase().includes(search.toLowerCase()))return true;
+    else if(part.partNumber.includes(search)) return true;
+  })
+  
+  const handleSearch = (searchInput) => {
+    
+    setSearch(searchInput)
+    
+  }
   return (
     <div className="App">
       <Navbar />
       <Routes>
-        <Route path='/inventory' element={<Inventory parts={parts}/>}>
+        <Route path='/inventory' element={<Inventory parts={updatedParts} onSearch={handleSearch}/>}>
         </Route>
         <Route path='/form' element={<Form />}>
         </Route>
