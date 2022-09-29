cc2109f00d6d83f55f6663402a80f5cc75984091
Aletha Wheeless <augustkama98@gmail.com>
2022-09-29 13:30:13 -1000

    adjust Part.js code

--- src/Part.js
+++ src/Part.js
@@ -1,19 +1,13 @@
 import React from "react";
 
 function Part({ name, partNumber, price, category }) {
-   
     return (
-        <>
-            <tr>
-                <td>{name}</td>
-           
-                <td>{partNumber}</td>
-            
-                <td>$ {price}</td>
-           
-                <td>{category}</td>
-            </tr>
-        </> 
+        <tr>
+            <td>{name}</td>
+            <td>{partNumber}</td>
+            <td>${price}</td>
+            <td>{category}</td>
+        </tr>
     )
 }
 export default Part;
