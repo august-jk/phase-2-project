c444794f5fae0445e7fe5dbf05c1f621c118b4fc
Aletha Wheeless <augustkama98@gmail.com>
2022-09-29 13:24:31 -1000

    adjust code layout

--- src/Navbar.js
+++ src/Navbar.js
@@ -5,10 +5,10 @@ function Navbar() {
     return (
         <div >
             <ul className="navbar">
-        <li><NavLink to="/">Home</NavLink></li>
-        <li><NavLink to='/inventory'>Inventory</NavLink></li>
-        <li><NavLink to='/form'>Add Parts</NavLink></li>
-      </ul>
+                <li><NavLink to="/">Home</NavLink></li>
+                <li><NavLink to='/inventory'>Inventory</NavLink></li>
+                <li><NavLink to='/form'>Add Parts</NavLink></li>
+            </ul>
         </div>
     )
 }
